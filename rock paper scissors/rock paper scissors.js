769f6ae9dc2f2cf2ae2f970d6a858dc474960c07
Koded <sogoseriki@gmail.com>
2023-03-09 16:23:03 +0100

    add

---
+++ rock paper scissors/rock paper scissors.js
@@ -0,0 +1,74 @@
+
+///////////////////////Function to get computer choice
+function getComputerChoice(){
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+     if(randomNum == 1){
+         return "rock"
+     }
+     else if(randomNum == 2){
+         return "paper"
+     }
+     else{
+         return "scissors"
+     }
+    
+}
+
+function getPlayerChoice(){
+let playerInput = prompt("Enter your choice: rock, paper, or scissors");
+return playerInput;
+}
+let playerSelection = getPlayerChoice(1)
+let computerSelection = getComputerChoice()
+console.log(computerSelection)
+console.log(playerSelection)
+
+function gameround(computerSelection,playerSelection){
+    if(computerSelection == playerSelection){
+        return("a tie")
+    }
+    else if(playerSelection =="rock" && computerSelection =="scissors"){
+        return("player wins")
+    }
+
+    else if(playerSelection =="paper" && computerSelection =="rock"){
+       return("player wins")
+    }
+
+    else if(playerSelection =="scissors" && computerSelection =="paper"){
+       return("player wins")
+    }
+
+   else{
+    return "computer wins"
+   }
+}
+function game(){
+    let playerScore = 0;
+    let computerScore =0;
+    
+for(let i = 0; i<=5; i++){
+     let computerSelection =getComputerChoice()
+     let playerSelection = getPlayerChoice()
+   let round = gameround(computerSelection, playerSelection)
+   if(round == ("a tie")){
+   }
+   else if(round ==("player wins")){
+     playerScore++  
+   } 
+   else {
+     computerScore++
+   }
+}
+  if (computerScore>playerScore){
+    return "COMPUTER WINS with "+ computerScore + " points"
+  }
+  else if(computerScore<playerScore){
+    return'PLAYER WINS with ' + playerScore + " points"
+  }
+  else{
+    return "it's a tie, computer got " + computerScore + " and player got " + playerScore
+  }
+ 
+}
+console.log(game())
